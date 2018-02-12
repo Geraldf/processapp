9df624f1ebd4fbc610d296d10d1360caeb2e6413
Gerald Fuchs <gerald@fuchsclan.de>
2018-02-12 16:24:02 +0100

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { Page404Component } from './page404/page404.component';
+import { ProcessesComponent } from './processes/processes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent
+  },
+  {
+    path: 'processes',
+    component: ProcessesComponent
+  },
+  {
+    path: '**',
+    component: Page404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { Page404Component } from './page404/page404.component';
+import { ProcessesComponent } from './processes/processes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    Page404Component,
+    ProcessesComponent
   ],
   imports: [
     BrowserModule,
